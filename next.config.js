014094bd7e43fcc9e2739c3df0e5155d16c7375a
Alx <112096089+Le-Xandre@users.noreply.github.com>
2025-06-12 22:33:23 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
-/** @type {import('next').NextConfig} */
+// next.config.js
 const nextConfig = {
   output: 'export',
   basePath: '/mon-portfolio',
   assetPrefix: '/mon-portfolio/',
   trailingSlash: true,
+  publicRuntimeConfig: {
+    basePath: '/mon-portfolio',
+  },
 };
 
 module.exports = nextConfig;
