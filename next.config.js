db53d6c59b5ab17e0a2d689c1577852e99959034
Alx <112096089+Le-Xandre@users.noreply.github.com>
2026-08-07 09:47:28 +0200

    Refactor next.config.js by removing unused settings
    Removed GitHub Pages specific configurations and comments.

--- next.config.js
+++ next.config.js
@@ -1,36 +1,30 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubPages = process.env.GITHUB_ACTIONS === "true";
-
 const nextConfig = {
   reactStrictMode: true,
 
-  ...(isGithubPages && {
-    output: "export",
-    trailingSlash: true,
-  }),
-
   images: {
-    unoptimized: isGithubPages,
-
     formats: ["image/avif", "image/webp"],
 
     /*
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "live.staticflickr.com",
-      },
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-    */
+     * Décommente uniquement si tu affiches
+     * des images hébergées sur un autre domaine.
+     *
+     * remotePatterns: [
+     *   {
+     *     protocol: "https",
+     *     hostname: "live.staticflickr.com",
+     *   },
+     *   {
+     *     protocol: "https",
+     *     hostname: "i.ytimg.com",
+     *   },
+     *   {
+     *     protocol: "https",
+     *     hostname: "images.unsplash.com",
+     *   },
+     * ],
+     */
   },
 
   webpack(config) {
