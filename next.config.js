2602fcf9f06b7b61404ea0be04855813fb422a20
Alx <112096089+Le-Xandre@users.noreply.github.com>
2026-08-07 09:45:16 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,41 +5,34 @@ const isGithubPages = process.env.GITHUB_ACTIONS === "true";
 const nextConfig = {
   reactStrictMode: true,
 
-  // Configuration spécifique à GitHub Pages
   ...(isGithubPages && {
     output: "export",
     trailingSlash: true,
-    images: {
-      unoptimized: true,
-    },
   }),
 
-  // Configuration des images
   images: {
+    unoptimized: isGithubPages,
+
     formats: ["image/avif", "image/webp"],
 
     /*
-     * Décommente uniquement si tu utilises
-     * des images hébergées à l'extérieur.
-     *
-     * remotePatterns: [
-     *   {
-     *     protocol: "https",
-     *     hostname: "live.staticflickr.com",
-     *   },
-     *   {
-     *     protocol: "https",
-     *     hostname: "i.ytimg.com",
-     *   },
-     *   {
-     *     protocol: "https",
-     *     hostname: "images.unsplash.com",
-     *   },
-     * ],
-     */
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "live.staticflickr.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+    */
   },
 
-  // Support des fichiers SVG en composants React
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
