4f1e3e6ca0bf354b98808b68276fdc577cc57519
Alx <112096089+Le-Xandre@users.noreply.github.com>
2026-08-10 10:36:31 +0200

    Update copyright year and text in Footer component

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
       <footer className="flex-shrink-0 py-2 bg-gray-200 dark:bg-gray-900 py-2">
           <div className="max-w-6xl mx-auto text-center text-gray-600 dark:text-gray-400">
-        &copy; 2025 MonPortfolio. Tous droits réservés.
+        &copy; 2025-2026 Mon Portfolio ♥ .Tous droits réservés.
           </div>
           <Iain04 />
     </footer>
